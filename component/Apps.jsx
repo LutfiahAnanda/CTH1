705956e06314188178594d4d6bc4f2790827c22e
LutfiahAnanda <lutfiahananda17@upi.edu>
2022-03-23 09:21:56 +0700

    first commit

---
+++ component/Apps.jsx
@@ -0,0 +1,12 @@
+import Home from "./Home";
+import './Apps.css';
+
+function App() {
+    return(
+        <div className="App">
+            <Home />
+        </div>
+    )
+}
+
+export default App;
