24aa5b8c597cff596efa24d51606115b82a070b9
LutfiahAnanda <lutfiahananda17@upi.edu>
2022-03-24 22:48:28 +0700

    first commit

---
+++ component 1/Body.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <><nav className="Home"></nav><><div>
+            <form className="form-search">
+                <input type="text" placeholder="Search..." className="form-search__input" required />
+                <button ype="submit" className="form-search__button">search</button>
+            </form>
+
+            <img className='image' src='https://i.scdn.co/image/ab67616d00004851e8b066f70c206551210d902b' alt='photo' />
+        </div><div>
+                <form className="form-search">
+                    <input type="text" placeholder="Search..." className="form-search__input" required />
+                    <button ype="submit" className="form-search__button">search</button>
+                </form>
+
+                <img className='image' src='https://i.scdn.co/image/ab67616d00004851e8b066f70c206551210d902b' alt='photo' />
+
+                <p>Name: Bohemian Rhapsody (The Original Soundtrack)</p>
+                <p>Release Date: 19-10-2018</p>
+                <p>Release Date Precision: Day</p>
+                <p>Album</p>
+            </div></></>
+    )
+};
---
+++ component/Page/Home/Stylesheet.jsx
@@ -0,0 +1,18 @@
+import React from 'react' ;
+import ReactDOM from 'react';
+
+class Header extends React.Component {
+    render() {
+        const stylesheet = {
+            color: "white",
+            backgroundColor:"Gray",
+            padding: "10px",
+            fontFamily: "Calibri"
+        };
+        Return(
+            <div>
+            <h1 style={{backgroundColor: "lightgray"}} font>Musik Playlist!</h1>
+            </div>
+        );
+    }
+};
